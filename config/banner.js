43308b8cc655f4394529563c3a991a8c83d11a89
malaata <tickpad8@gmail.com>
2024-12-22 17:10:13 +0800

    Create banner.js

---
+++ config/banner.js
@@ -0,0 +1,29 @@
+const figlet = require("figlet");
+const { ColorTheme } = require("./colors");
+
+const colors = new ColorTheme();
+
+function displayBanner() {
+  // 使用 figlet 库生成横幅文字
+  const banner = figlet.textSync("Humanity BOT", {
+    font: "ANSI Shadow", // 设置字体
+    horizontalLayout: "default", // 水平布局方式
+    verticalLayout: "default", // 垂直布局方式
+    width: 100, // 设置横幅宽度
+  });
+
+  // 输出横幅的样式
+  console.log(colors.style(banner, "header"));
+  console.log(
+    colors.style("===============================================", "border") // 输出分隔符
+  );
+  console.log(colors.style("XXX", "link")); // 输出编写者信息
+  console.log(colors.style("XXX", "link")); // 输出电报频道链接
+  console.log(
+    colors.style("===============================================\n", "border") // 输出分隔符
+  );
+}
+
+module.exports = displayBanner;
+
+
